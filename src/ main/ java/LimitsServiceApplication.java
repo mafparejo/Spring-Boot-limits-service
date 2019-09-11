bf068ce5a4572fd79bad3078c62a5c498e327e55
mafparejo <45272983+mafparejo@users.noreply.github.com>
2019-09-11 12:45:51 +0200

    Update and rename src/ main/ java/LimitsServiceApplication.java to  src/ main/ java/LimitsServiceApplication.java

--- src/ main/ java/LimitsServiceApplication.java
+++ src/ main/ java/LimitsServiceApplication.java
@@ -1,1 +1,13 @@
+paquete  com.microservicios.parejo.limitsservice ;
 
+importar  org.springframework.boot.SpringApplication ;
+importar  org.springframework.boot.autoconfigure.SpringBootApplication ;
+
+@SpringBootApplication
+public  class  LimitsServiceApplication {
+
+	public  static  void  main ( String [] args ) {
+		SpringApplication . ejecutar ( LimitsServiceApplication . class, args);
+	}
+
+}
