b9ca2b72765f6b6b4e798454d316e13295386290
mafparejo <45272983+mafparejo@users.noreply.github.com>
2019-09-11 12:48:10 +0200

    Update and rename src/ main/ java/Configuration.java to  src/ main/ java/Configuration.java

--- src/ main/ java/Configuration.java
+++ src/ main/ java/Configuration.java
@@ -1,1 +1,41 @@
+paquete  com.microservicios.parejo.limitsservice ;
 
+importar  org.springframework.boot.context.properties.ConfigurationProperties ;
+import  org.springframework.stereotype.Component ;
+
+@Componente
+@ConfigurationProperties ( " limites-servicio " )
+ Configuración de clase  pública {
+
+	privado  int minimo;
+	privada  int maximo;
+	 Cadena privada nombre;
+	 sino booleano privado ;
+	
+	public  String  getNombre () {
+		volver nombre;
+	}
+	public  void  setNombre ( String  nombre ) {
+		esta . nombre = nombre;
+	}
+	public  int  getMinimo () {
+		retorno minimo;
+	}
+	public  void  setMinimo ( int  minimo ) {
+		esta . minimo = minimo;
+	}
+	public  int  getMaximo () {
+		volver maximo;
+	}
+	public  void  setMaximo ( int  maximo ) {
+		esta . maximo = maximo;
+	}
+	
+	public  boolean  isSino () {
+		volver sino;
+	}
+	público  vacío  setSino ( sino booleano  ) {
+		esta . sino = sino;
+	}
+	
+}
