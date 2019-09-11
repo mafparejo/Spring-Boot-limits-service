a204a6ef4913969cee7e57e36f6687c271f50381
mafparejo <45272983+mafparejo@users.noreply.github.com>
2019-09-11 12:43:13 +0200

    Update and rename src/ main/ java/LimitsConfigurationController.java to  src/ main/ java/LimitsConfigurationController.java

--- src/ main/ java/LimitsConfigurationController.java
+++ src/ main/ java/LimitsConfigurationController.java
@@ -1,1 +1,22 @@
+paquete  com.microservicios.parejo.limitsservice ;
 
+import  org.springframework.beans.factory.annotation.Autowired ;
+import  org.springframework.web.bind.annotation.GetMapping ;
+import  org.springframework.web.bind.annotation.RestController ;
+
+@RestController
+public  class  LimitsConfigurationController {
+
+	@Autowired
+	 configuración de configuración privada ;
+	
+	
+	@GetMapping ( " / limites " )
+	public  LimitConfiguration  recuperarLimiteConfiguration () {
+		boolean recuperoBoolean = config . isSino ();
+		if (recuperoBoolean) {
+			return  new  LimitConfiguration (config . isSino ());
+		}
+		return  new  LimitConfiguration (config . getMaximo (), config . getMinimo (), config . getNombre (), config . isSino ());
+	}
+}
